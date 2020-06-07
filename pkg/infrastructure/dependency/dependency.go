11b9c45e3d866438794dd8c9ca6468aa58c75f64
Phodal Huang <h@phodal.com>
2020-06-07 21:44:51 +0800

    feat: add artifact id support

--- pkg/infrastructure/dependency/dependency.go
+++ pkg/infrastructure/dependency/dependency.go
@@ -5,14 +5,16 @@ import (
 )
 
 type Dependency struct {
-	Version string
+	Version    string
+	ArtifactId string
 }
 
 func ByFileName(s string) Dependency {
 	var dependency Dependency
-	reg := regexp.MustCompile("([a-z][a-z0-9_-])*-([.a-z0-9_]+[.0-9a-z_]*).jar")
+	reg := regexp.MustCompile("([a-z][a-z0-9_-]*)-([.a-z0-9_]+[.0-9a-z_]*).jar")
 	result := reg.FindStringSubmatch(s)
 	if len(result) >= 2 {
+		dependency.ArtifactId = result[1]
 		dependency.Version = result[2]
 	}
 
--- pkg/infrastructure/dependency/dependency_test.go
+++ pkg/infrastructure/dependency/dependency_test.go
@@ -5,10 +5,16 @@ import (
 	"testing"
 )
 
-func TestShouldGetProjectName(t *testing.T) {
+func TestShouldGetVersionByName(t *testing.T) {
 	g := NewGomegaWithT(t)
 
 	result := ByFileName(`pax-exam-1.1.0.jar`)
 	g.Expect(result.Version).To(Equal(`1.1.0`))
 }
 
+func TestShouldGetArtifactId(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	result := ByFileName(`pax-exam-1.1.0.jar`)
+	g.Expect(result.ArtifactId).To(Equal(`pax-exam`))
+}
