bf5eb5ad35838dfe92981ba702f234f36142c47b
Phodal Huang <h@phodal.com>
2020-06-07 23:53:47 +0800

    feat: try to add unzip for jar

--- pkg/infrastructure/bundle/jar_test.go
+++ pkg/infrastructure/bundle/jar_test.go
@@ -1,6 +1,7 @@
 package bundle
 
 import (
+	"fmt"
 	. "github.com/onsi/gomega"
 	"path/filepath"
 	"testing"
@@ -31,4 +32,17 @@ func TestShouldReadZipList(t *testing.T) {
 		"META-INF/maven/org.vaadin.example.osgi/felix-tut-8/pom.xml",
 		"META-INF/maven/org.vaadin.example.osgi/felix-tut-8/pom.properties",
 	}))
+}
+
+func TestShouldUnzipJar(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	path := `../../../_fixtures/bundle/sample/felix-tut-8-1.0-SNAPSHOT.jar`
+	absPath := filepath.FromSlash(path)
+	err := Unzip(absPath, filepath.FromSlash("./tmp"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	g.Expect(".").To(Equal((".")))
 }
