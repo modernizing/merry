711ece2a25f366c783ed8734d88c8306001616c5
Phodal Huang <h@phodal.com>
2020-06-07 23:37:07 +0800

    feat: add file hash

---
+++ pkg/infrastructure/bundle/checksum_test.go
@@ -0,0 +1,18 @@
+package bundle
+
+import (
+	. "github.com/onsi/gomega"
+	"path/filepath"
+	"testing"
+)
+
+func TestShouldGetPropertyName(t *testing.T) {
+	g := NewGomegaWithT(t)
+	path := `../../../_fixtures/bundle/sample/felix-tut-8-1.0-SNAPSHOT.jar`
+	absPath := filepath.FromSlash(path)
+
+	result, _ := HashFileMD5(absPath)
+	g.Expect(result).To(Equal(`49228cf9d883ca8343234b895dfb3623`))
+}
+
+
