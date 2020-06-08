6ea4fd6656fcf2ac9f24a9e7b9d5b94c34164b01
Phodal Huang <h@phodal.com>
2020-06-08 15:14:59 +0800

    feat: add basic convert

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.13
 require (
 	github.com/onsi/ginkgo v1.12.3
 	github.com/onsi/gomega v1.10.1
-	github.com/urfave/cli/v2 v2.2.0 // indirect
+	github.com/urfave/cli/v2 v2.2.0
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
 	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
 	google.golang.org/protobuf v1.24.0 // indirect
--- igso.go
+++ igso.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/phodal/igso/pkg/application/maven"
 	"github.com/urfave/cli/v2"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 
@@ -13,7 +16,36 @@ func main() {
 		Name: "boom",
 		Usage: "make an explosive entrance",
 		Action: func(c *cli.Context) error {
-			fmt.Println("boom! I say!")
+			var contents []byte
+			var err error
+			if c.Args().Len() > 1 {
+				path := c.Args().Get(1)
+				fmt.Println(path)
+				contents, err = ioutil.ReadFile(filepath.FromSlash(path + "/" + "build.xml"))
+			} else {
+				contents, err = ioutil.ReadFile("build.xml")
+			}
+			if err != nil {
+				fmt.Println(err)
+				_ = fmt.Errorf("Failed read file: %s ", err)
+				return nil
+			}
+
+			deps := maven.FromAnt(string(contents))
+
+			var results = ""
+			for _, dep := range deps {
+				var tmpl = `
+      <dependency>
+          <groupId>` + dep.GroupId + `</groupId>
+          <artifactId>` + dep.ArtifactId + `</artifactId>
+          <version>` + dep.Version + `</version>
+      </dependency>
+`
+				results += tmpl
+			}
+
+			fmt.Println(results)
 			return nil
 		},
 	}
