6f91d8efe808210913ffa4f2ac3ebc2599f991df
Phodal Huang <h@phodal.com>
2020-06-08 17:30:28 +0800

    feat: add get property form zip

--- pkg/infrastructure/properties/properties.go
+++ pkg/infrastructure/properties/properties.go
@@ -1,13 +1,67 @@
 package properties
 
 import (
+	"archive/zip"
 	"bufio"
+	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 )
 
 type AppConfigProperties map[string]string
+
+type myCloser interface {
+	Close() error
+}
+// check is a helper function which streamlines error checking
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// closeFile is a helper function which streamlines closing
+// with error checking on different file types.
+func closeFile(f myCloser) {
+	err := f.Close()
+	check(err)
+}
+
+// readAll is a wrapper function for ioutil.ReadAll. It accepts a zip.File as
+// its parameter, opens it, reads its content and returns it as a byte slice.
+func readAll(file *zip.File) []byte {
+	fc, err := file.Open()
+	check(err)
+	defer closeFile(fc)
+
+	content, err := ioutil.ReadAll(fc)
+	check(err)
+
+	return content
+}
+
+
+func ReadPropertiesFromZip(zipFile string) (AppConfigProperties, error) {
+	config := AppConfigProperties{}
+
+	zf, err := zip.OpenReader(zipFile)
+	check(err)
+	defer closeFile(zf)
+
+	for _, file := range zf.File {
+		if strings.HasSuffix(file.Name, "pom.properties") {
+			all := readAll(file)
+			content := string(all)
+			lines := strings.Split(content, "\n")
+			for _, line := range lines {
+				buildConfigByLine(line, config)
+			}
+		}
+	}
+
+	return config, err
+}
 
 // https://stackoverflow.com/questions/40022861/parsing-values-from-property-file-in-golang
 func ReadPropertiesFile(filename string) (AppConfigProperties, error) {
@@ -26,15 +80,7 @@ func ReadPropertiesFile(filename string) (AppConfigProperties, error) {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		if equal := strings.Index(line, "="); equal >= 0 {
-			if key := strings.TrimSpace(line[:equal]); len(key) > 0 {
-				value := ""
-				if len(line) > equal {
-					value = strings.TrimSpace(line[equal+1:])
-				}
-				config[key] = value
-			}
-		}
+		buildConfigByLine(line, config)
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -43,4 +89,16 @@ func ReadPropertiesFile(filename string) (AppConfigProperties, error) {
 	}
 
 	return config, nil
+}
+
+func buildConfigByLine(line string, config AppConfigProperties) {
+	if equal := strings.Index(line, "="); equal >= 0 {
+		if key := strings.TrimSpace(line[:equal]); len(key) > 0 {
+			value := ""
+			if len(line) > equal {
+				value = strings.TrimSpace(line[equal+1:])
+			}
+			config[key] = value
+		}
+	}
 }
--- pkg/infrastructure/properties/properties_test.go
+++ pkg/infrastructure/properties/properties_test.go
@@ -1,6 +1,7 @@
 package properties
 
 import (
+	"fmt"
 	. "github.com/onsi/gomega"
 	"path/filepath"
 	"testing"
@@ -12,6 +13,18 @@ func TestShouldGetPropertyName(t *testing.T) {
 	absPath := filepath.FromSlash(path)
 
 	result, _ := ReadPropertiesFile(absPath)
+	g.Expect(result["version"]).To(Equal("13.0"))
+	g.Expect(result["groupId"]).To(Equal("org.jetbrains"))
+	g.Expect(result["artifactId"]).To(Equal("annotations"))
+}
+
+func TestShouldGetPropertyFromZip(t *testing.T) {
+	g := NewGomegaWithT(t)
+	path := `../../../_fixtures/jar/sample/annotations-13.0.jar`
+	absPath := filepath.FromSlash(path)
+
+	result, _ := ReadPropertiesFromZip(absPath)
+	fmt.Println(result)
 	g.Expect(result["version"]).To(Equal("13.0"))
 	g.Expect(result["groupId"]).To(Equal("org.jetbrains"))
 	g.Expect(result["artifactId"]).To(Equal("annotations"))
