c3bbf804ba7cb5c657cbfa095494839e95b026e7
Phodal Huang <h@phodal.com>
2020-06-09 22:56:52 +0800

    feat: add checksum for result

--- cmd/boom.go
+++ cmd/boom.go
@@ -1,1 +1,49 @@
 package cmd
+
+import (
+	"fmt"
+	"github.com/phodal/igso/pkg/application/maven"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+type BoomConfig struct {
+	Path    string
+	WithGit bool
+}
+
+var (
+	boomConfig BoomConfig
+)
+
+
+func init() {
+	boomCmd.SetOut(output)
+
+	boomCmd.PersistentFlags().StringVarP(&boomConfig.Path, "path", "p", ".", "path")
+	boomCmd.PersistentFlags().BoolVarP(&boomConfig.WithGit, "extract", "x", false, "extract file")
+
+	rootCmd.AddCommand(boomCmd)
+}
+
+var boomCmd = &cobra.Command{
+	Use:   "boom",
+	Short: "b",
+	Run: func(cmd *cobra.Command, args []string) {
+		var contents []byte
+		var err error
+		path := cmd.Flag("path").Value.String()
+
+		contents, err = ioutil.ReadFile(filepath.FromSlash(path + "/" + "build.xml"))
+		if err != nil {
+			fmt.Println(err)
+			_ = fmt.Errorf("Failed read file: %s ", err)
+		}
+
+		withPom := maven.FromAntToXml(string(contents), boomConfig.WithGit)
+
+		_ = ioutil.WriteFile(filepath.FromSlash(path+"/pom.xml"), []byte(withPom), os.ModePerm)
+	},
+}
--- igso.go
+++ igso.go
@@ -25,25 +25,6 @@ func main2() {
 	app.UseShortOptionHandling = true
 	app.Commands = []*cli.Command{
 		{
-			Name:  "boom",
-			Usage: "build maven dep from build.xml",
-			Flags: []cli.Flag{
-				&cli.StringFlag{
-					Name: "path", Aliases: []string{"p"},
-				},
-				&cli.BoolFlag{
-					Name: "extract", Aliases: []string{"x"},
-				},
-			},
-			Action: func(c *cli.Context) error {
-				err, done := outputResult(c)
-				if done {
-					return err
-				}
-				return nil
-			},
-		},
-		{
 			Name:  "dupsearch",
 			Usage: "search unkonw jar source",
 			Flags: []cli.Flag{
@@ -92,30 +73,4 @@ func main2() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func outputResult(c *cli.Context) (error, bool) {
-	var contents []byte
-	var err error
-	var path = "."
-	if c.String("path") != "" {
-		path = c.String("path")
-	}
-
-	isExtract := c.Bool("extract")
-	if c.Args().Len() > 1 {
-		contents, err = ioutil.ReadFile(filepath.FromSlash(path + "/" + "build.xml"))
-	} else {
-		contents, err = ioutil.ReadFile("build.xml")
-	}
-	if err != nil {
-		fmt.Println(err)
-		_ = fmt.Errorf("Failed read file: %s ", err)
-		return nil, true
-	}
-
-	withPom := maven.FromAntToXml(string(contents), isExtract)
-
-	_ = ioutil.WriteFile(filepath.FromSlash(path+"/pom.xml"), []byte(withPom), os.ModePerm)
-	return nil, false
 }
