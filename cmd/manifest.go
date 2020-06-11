f9b83ab6cff5944ce72163df735aa4e5af943425
Phodal Huang <h@phodal.com>
2020-06-11 12:58:11 +0800

    refactor: move extract manifest to cmd

--- cmd/manifest.go
+++ cmd/manifest.go
@@ -1,8 +1,14 @@
 package cmd
 
 import (
-	"github.com/phodal/igso/pkg/application/manifest"
+	"fmt"
+	"github.com/phodal/igso/pkg/infrastructure/bundle"
+	"github.com/phodal/igso/pkg/instrastructure"
 	"github.com/spf13/cobra"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 type ManifestConfig struct {
@@ -28,7 +34,25 @@ var manifestCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		path := cmd.Flag("path").Value.String()
 		if manifestConfig.Extract {
-			manifest.ExtractManifest(path)
+			ExtractManifest(path)
 		}
 	},
+}
+
+func ExtractManifest(ppath string) {
+	var jarFileFilter = func(path string) bool {
+		return strings.HasSuffix(path, ".jar")
+	}
+
+	jarPaths := instrastructure.GetFilesByFilter(ppath, jarFileFilter)
+	fmt.Println(ppath)
+	for _, path := range jarPaths {
+		_, content, _ := bundle.GetFileFromJar(path, "MANIFEST.MF")
+		pureName := path[len(ppath)+1 : len(path)-len(".jar")]
+
+		filePath := "_m/" + pureName + "/META-INF/"
+
+		_ = os.MkdirAll(filepath.FromSlash(filePath), os.ModePerm)
+		_ = ioutil.WriteFile(filepath.FromSlash(filePath+"MANIFEST.MF"), []byte(content), os.ModePerm)
+	}
 }
