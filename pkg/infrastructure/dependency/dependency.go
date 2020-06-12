11cbf818545744f44c62ab04ed231004901a0b3e
Phodal Huang <h@phodal.com>
2020-06-12 15:33:29 +0800

    feat: add version verfiy

--- pkg/infrastructure/dependency/dependency.go
+++ pkg/infrastructure/dependency/dependency.go
@@ -5,9 +5,21 @@ import (
 	"regexp"
 )
 
-func ByFileName(s string) MavenDependency {
+func BySlashFileName(s string) MavenDependency {
 	var dependency MavenDependency
 	reg := regexp.MustCompile("([a-z][a-z0-9_-]*)-([.a-z0-9_]+[.0-9a-z_]*).jar")
+	result := reg.FindStringSubmatch(s)
+	if len(result) >= 2 {
+		dependency.ArtifactId = result[1]
+		dependency.Version = result[2]
+	}
+
+	return dependency
+}
+
+func ByFileName(s string) MavenDependency {
+	var dependency MavenDependency
+	reg := regexp.MustCompile("([.a-z0-9]+[.0-9a-z]*)_([.a-z0-9]+[.0-9a-z]*).jar")
 	result := reg.FindStringSubmatch(s)
 	if len(result) >= 2 {
 		dependency.ArtifactId = result[1]
--- pkg/infrastructure/dependency/dependency_test.go
+++ pkg/infrastructure/dependency/dependency_test.go
@@ -8,13 +8,20 @@ import (
 func TestShouldGetVersionByName(t *testing.T) {
 	g := NewGomegaWithT(t)
 
-	result := ByFileName(`pax-exam-1.1.0.jar`)
+	result := BySlashFileName(`pax-exam-1.1.0.jar`)
 	g.Expect(result.Version).To(Equal(`1.1.0`))
 }
 
 func TestShouldGetArtifactId(t *testing.T) {
 	g := NewGomegaWithT(t)
 
-	result := ByFileName(`pax-exam-1.1.0.jar`)
+	result := BySlashFileName(`pax-exam-1.1.0.jar`)
 	g.Expect(result.ArtifactId).To(Equal(`pax-exam`))
+}
+
+func Test_ShouldSupportCustomPackageNameLength(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	result := ByFileName(`com.csii.pe.http_6.0.0.20121109.jar`)
+	g.Expect(result.Version).To(Equal(`6.0.0.20121109`))
 }
