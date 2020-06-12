e395e5f7b8531cf4b76dd37bdee02940c99e0d47
Phodal Huang <h@phodal.com>
2020-06-12 15:39:02 +0800

    feat: add parser for filename

--- pkg/infrastructure/dependency/dependency.go
+++ pkg/infrastructure/dependency/dependency.go
@@ -3,6 +3,7 @@ package dependency
 import (
 	. "github.com/phodal/igso/pkg/domain"
 	"regexp"
+	"strings"
 )
 
 func BySlashFileName(s string) MavenDependency {
@@ -17,12 +18,18 @@ func BySlashFileName(s string) MavenDependency {
 	return dependency
 }
 
-func ByFileName(s string) MavenDependency {
+func ByFileName(s string, groupIdLength int) MavenDependency {
 	var dependency MavenDependency
 	reg := regexp.MustCompile("([.a-z0-9]+[.0-9a-z]*)_([.a-z0-9]+[.0-9a-z]*).jar")
 	result := reg.FindStringSubmatch(s)
 	if len(result) >= 2 {
-		dependency.ArtifactId = result[1]
+		if strings.Contains(result[1], ".") {
+			split := strings.Split(result[1], ".")
+			dependency.GroupId = strings.Join(split[0:groupIdLength], ".")
+			dependency.ArtifactId = strings.Join(split[groupIdLength:], ".")
+		} else {
+			dependency.ArtifactId = result[1]
+		}
 		dependency.Version = result[2]
 	}
 
--- pkg/infrastructure/dependency/dependency_test.go
+++ pkg/infrastructure/dependency/dependency_test.go
@@ -22,6 +22,22 @@ func TestShouldGetArtifactId(t *testing.T) {
 func Test_ShouldSupportCustomPackageNameLength(t *testing.T) {
 	g := NewGomegaWithT(t)
 
-	result := ByFileName(`com.csii.pe.http_6.0.0.20121109.jar`)
+	result := ByFileName(`com.csii.pe.http_6.0.0.20121109.jar`, 0)
 	g.Expect(result.Version).To(Equal(`6.0.0.20121109`))
+}
+
+func Test_ShouldGetTwoLengthGroupIdWhen2(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	result := ByFileName(`com.csii.pe.http_6.0.0.20121109.jar`, 2)
+	g.Expect(result.GroupId).To(Equal(`com.csii`))
+	g.Expect(result.ArtifactId).To(Equal(`pe.http`))
+}
+
+func Test_ShouldGetOneLengthGroupIdWhen2(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	result := ByFileName(`com.csii.pe.http_6.0.0.20121109.jar`, 1)
+	g.Expect(result.GroupId).To(Equal(`com`))
+	g.Expect(result.ArtifactId).To(Equal(`csii.pe.http`))
 }
