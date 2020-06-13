9b27e251c4f8ceeae53d1eb762c5abf2d86fcb88
Phodal Huang <h@phodal.com>
2020-06-13 18:23:44 +0800

    fix: fix path issue for windows

--- pkg/infrastructure/file_test.go
+++ pkg/infrastructure/file_test.go
@@ -3,12 +3,13 @@ package infrastructure
 
 import (
 	. "github.com/onsi/gomega"
+	"path/filepath"
 	"testing"
 )
 
 func TestShouldGetProjectName(t *testing.T) {
 	g := NewGomegaWithT(t)
-	path := GetJarPath("com/phodal/hello.jar")
+	path := GetJarPath(filepath.FromSlash("com/phodal/hello.jar"))
 
-	g.Expect(path).To(Equal(`com/phodal/`))
+	g.Expect(path).To(Equal(filepath.FromSlash(`com/phodal/`)))
 }
