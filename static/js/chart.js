7d595bdf730a1f825224d728f89c7310eb08aaa1
Phodal Huang <h@phodal.com>
2020-06-14 23:18:56 +0800

    refactor: extract chart method

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,159 +1,162 @@
-// d3.json("output.json").then(function (data) {
+function renderVertical() {
+    return function (data) {
+        var width = 1440;
+        var height = 2048;
 
-d3.json("/manifest-map.json").then(function (data) {
-    console.log(data);
-    var width = 1440;
-    var height = 2048;
+        const nodes = data.nodes.map(({id, group}) => ({
+            id,
+            sourceLinks: [],
+            targetLinks: [],
+            group
+        }));
 
-    const nodes = data.nodes.map(({id, group}) => ({
-        id,
-        sourceLinks: [],
-        targetLinks: [],
-        group
-    }));
+        const nodeById = new Map(nodes.map(d => [d.id, d]));
 
-    const nodeById = new Map(nodes.map(d => [d.id, d]));
+        const links = data.links.map(({source, target, value}) => ({
+            source: nodeById.get(source),
+            target: nodeById.get(target),
+            value
+        }));
 
-    const links = data.links.map(({source, target, value}) => ({
-        source: nodeById.get(source),
-        target: nodeById.get(target),
-        value
-    }));
-
-    for (const link of links) {
-        const {source, target, value} = link;
-        if (target && source) {
-            source.sourceLinks.push(link);
-            target.targetLinks.push(link);
+        for (const link of links) {
+            const {source, target, value} = link;
+            if (target && source) {
+                source.sourceLinks.push(link);
+                target.targetLinks.push(link);
+            }
         }
-    }
 
-    var margin = ({top: 20, right: 20, bottom: 20, left: 250});
-    var step = 14;
+        var margin = ({top: 20, right: 20, bottom: 20, left: 250});
+        var step = 14;
 
-    height = (nodes.length - 1) * step + margin.top + margin.bottom
-    var y = d3.scalePoint(nodes.map(d => d.id).sort(d3.ascending), [margin.top, height - margin.bottom])
-    var color = d3.scaleOrdinal(nodes.map(d => d.group).sort(d3.ascending), d3.schemeCategory10)
-    var svg = d3.select("#svg").append("svg")
-        .attr("width", width)
-        .attr("height", height);
+        height = (nodes.length - 1) * step + margin.top + margin.bottom
+        var y = d3.scalePoint(nodes.map(d => d.id).sort(d3.ascending), [margin.top, height - margin.bottom])
+        var color = d3.scaleOrdinal(nodes.map(d => d.group).sort(d3.ascending), d3.schemeCategory10)
+        var svg = d3.select("#svg").append("svg")
+            .attr("width", width)
+            .attr("height", height);
 
-    // Define the div for the tooltip
-    var div = d3.select("body").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
+        // Define the div for the tooltip
+        var div = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
 
-    const label = svg.append("g")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", 12)
-        .attr("text-anchor", "end")
-        .selectAll("g")
-        .data(nodes)
-        .join("g")
-        .attr("transform", d => `translate(${margin.left},${d.y = y(d.id)})`)
-        .call(g => g.append("text")
-            .attr("x", -6)
-            .attr("dy", "0.35em")
-            .attr("fill", d => d3.lab(color(d.group)).darker(2))
-            .text(d => d.id))
-        .call(g => g.append("circle")
-            .attr("r", 3)
-            .attr("fill", d => color(d.group)));
+        const label = svg.append("g")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 12)
+            .attr("text-anchor", "end")
+            .selectAll("g")
+            .data(nodes)
+            .join("g")
+            .attr("transform", d => `translate(${margin.left},${d.y = y(d.id)})`)
+            .call(g => g.append("text")
+                .attr("x", -6)
+                .attr("dy", "0.35em")
+                .attr("fill", d => d3.lab(color(d.group)).darker(2))
+                .text(d => d.id))
+            .call(g => g.append("circle")
+                .attr("r", 3)
+                .attr("fill", d => color(d.group)));
 
-    const path = svg.insert("g", "*")
-        .attr("fill", "none")
-        .attr("stroke-opacity", 0.6)
-        .attr("stroke-width", 1.5)
-        .selectAll("path")
-        .data(links)
-        .join("path")
-        .attr("stroke", d => d.source.group === d.target.group ? color(d.source.group) : "#aaa")
-        .attr("d", arc);
+        const path = svg.insert("g", "*")
+            .attr("fill", "none")
+            .attr("stroke-opacity", 0.6)
+            .attr("stroke-width", 1.5)
+            .selectAll("path")
+            .data(links)
+            .join("path")
+            .attr("stroke", d => d.source.group === d.target.group ? color(d.source.group) : "#aaa")
+            .attr("d", arc);
 
-    const overlay = svg.append("g")
-        .attr("fill", "none")
-        .attr("pointer-events", "all")
-        .selectAll("rect")
-        .data(nodes)
-        .join("rect")
-        .attr("width", margin.left + 40)
-        .attr("height", step)
-        .attr("y", d => y(d.id) - step / 2)
-        .on("mouseover", d => {
-            svg.classed("hover", true);
-            label.classed("primary", n => n === d);
-            label.classed("secondary", n => n.sourceLinks.some(l => l.target === d) || n.targetLinks.some(l => l.source === d));
-            path.classed("primary", l => l.source === d || l.target === d).filter(".primary").raise();
+        const overlay = svg.append("g")
+            .attr("fill", "none")
+            .attr("pointer-events", "all")
+            .selectAll("rect")
+            .data(nodes)
+            .join("rect")
+            .attr("width", margin.left + 40)
+            .attr("height", step)
+            .attr("y", d => y(d.id) - step / 2)
+            .on("mouseover", d => {
+                svg.classed("hover", true);
+                label.classed("primary", n => n === d);
+                label.classed("secondary", n => n.sourceLinks.some(l => l.target === d) || n.targetLinks.some(l => l.source === d));
+                path.classed("primary", l => l.source === d || l.target === d).filter(".primary").raise();
 
-            var result = "<h3>" + d.id + "</h3>";
-            for (let linkElement of d.targetLinks) {
-                result += "usedby: " + linkElement.source.id + "<br>"
-            }
+                var result = "<h3>" + d.id + "</h3>";
+                for (let linkElement of d.targetLinks) {
+                    result += "usedby: " + linkElement.source.id + "<br>"
+                }
 
-            for (let linkElement of d.sourceLinks) {
-                result += "use: " + linkElement.target.id + "<br>"
-            }
+                for (let linkElement of d.sourceLinks) {
+                    result += "use: " + linkElement.target.id + "<br>"
+                }
 
-            div.transition()
-                .duration(200)
-                .style("opacity", .9);
-            div.html(result)
-                .style("display", "block")
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", d => {
-            svg.classed("hover", false);
-            label.classed("primary", false);
-            label.classed("secondary", false);
-            path.classed("primary", false).order();
-            div.style("display", "none")
-        });
-
-    function update(order) {
-        y.domain(nodes.sort(order.value).map(d => d.id));
-
-        const t = svg.transition()
-            .duration(750);
-
-        label.transition(t)
-            .delay((d, i) => i * 20)
-            .attrTween("transform", d => {
-                const i = d3.interpolateNumber(d.y, y(d.id));
-                return t => `translate(${margin.left},${d.y = i(t)})`;
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                div.html(result)
+                    .style("display", "block")
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", d => {
+                svg.classed("hover", false);
+                label.classed("primary", false);
+                label.classed("secondary", false);
+                path.classed("primary", false).order();
+                div.style("display", "none")
             });
 
-        path.transition(t)
-            .duration(750 + nodes.length * 20)
-            .attrTween("d", d => () => arc(d));
+        function update(order) {
+            y.domain(nodes.sort(order.value).map(d => d.id));
 
-        overlay.transition(t)
-            .delay((d, i) => i * 20)
-            .attr("y", d => y(d.id) - step / 2);
-    }
+            const t = svg.transition()
+                .duration(750);
 
-    function arc(d) {
-        const y1 = d.source.y;
-        const y2 = d.target.y;
-        const r = Math.abs(y2 - y1) / 2;
-        return `M${margin.left},${y1}A${r},${r} 0,0,${y1 < y2 ? 1 : 0} ${margin.left},${y2}`;
-    }
+            label.transition(t)
+                .delay((d, i) => i * 20)
+                .attrTween("transform", d => {
+                    const i = d3.interpolateNumber(d.y, y(d.id));
+                    return t => `translate(${margin.left},${d.y = i(t)})`;
+                });
 
-    d3.select("#selectSort").on("change", function () {
-        var value = d3.select("#selectSort").node().value;
-        var selectMap = {
-            Group: {
-                value: (a, b) => a.group - b.group || d3.ascending(a.id, b.id)
-            },
-            Name: {
-                value: (a, b) => d3.ascending(a.id, b.id)
-            },
-            Frequency: {
-                value: (a, b) => d3.sum(b.sourceLinks, l => l.value) + d3.sum(b.targetLinks, l => l.value) - d3.sum(a.sourceLinks, l => l.value) - d3.sum(a.targetLinks, l => l.value) || d3.ascending(a.id, b.id)
-            }
+            path.transition(t)
+                .duration(750 + nodes.length * 20)
+                .attrTween("d", d => () => arc(d));
+
+            overlay.transition(t)
+                .delay((d, i) => i * 20)
+                .attr("y", d => y(d.id) - step / 2);
         }
 
-        let select = selectMap[value];
-        update(select);
-    })
-});
+        function arc(d) {
+            const y1 = d.source.y;
+            const y2 = d.target.y;
+            const r = Math.abs(y2 - y1) / 2;
+            return `M${margin.left},${y1}A${r},${r} 0,0,${y1 < y2 ? 1 : 0} ${margin.left},${y2}`;
+        }
+
+        d3.select("#selectSort").on("change", function () {
+            var value = d3.select("#selectSort").node().value;
+            var selectMap = {
+                Group: {
+                    value: (a, b) => a.group - b.group || d3.ascending(a.id, b.id)
+                },
+                Name: {
+                    value: (a, b) => d3.ascending(a.id, b.id)
+                },
+                Frequency: {
+                    value: (a, b) => d3.sum(b.sourceLinks, l => l.value) + d3.sum(b.targetLinks, l => l.value) - d3.sum(a.sourceLinks, l => l.value) - d3.sum(a.targetLinks, l => l.value) || d3.ascending(a.id, b.id)
+                }
+            }
+
+            let select = selectMap[value];
+            update(select);
+        })
+    };
+}
+
+d3.json("/manifest-map.json").then(renderVertical());
+d3.json("output.json").then(renderVertical());
+
