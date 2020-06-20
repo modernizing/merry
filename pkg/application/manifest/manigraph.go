74fea232d28fd762840d35ebebc5e4c6952f624d
Phodal Huang <h@phodal.com>
2020-06-20 11:17:57 +0800

    refactor: move te to infra

--- pkg/application/manifest/manigraph.go
+++ pkg/application/manifest/manigraph.go
@@ -2,8 +2,8 @@ package manifest
 
 import (
 	"fmt"
-	"github.com/phodal/merry/pkg/adapter/tequila"
 	"github.com/phodal/merry/pkg/domain"
+	"github.com/phodal/merry/pkg/infrastructure/tequila"
 )
 
 func BuildFullGraph(scanManifest []domain.IgsoManifest, depmap map[string]domain.MavenDependency) *tequila.FullGraph {
