9e6e15e0e7656c635426eab264f0cecdeb464258
Phodal Huang <h@phodal.com>
2020-06-21 21:57:59 +0800

    feat: add zoom support

--- static/js/chart.js
+++ static/js/chart.js
@@ -394,67 +394,88 @@ function renderPacking() {
 
         var pack = function (data) {
             return d3.pack()
-                .size([width - 2, height - 2])
+                .size([width, height])
                 .padding(3)
                 (d3.hierarchy(data)
                     .sum(d => d.value)
-                    .sort((a, b) => b.value - a.value));
+                    .sort((a, b) => b.value - a.value))
         }
 
         var width = 1200;
         var height = width;
         var format = d3.format(",d")
-        var color = d3.scaleSequential([8, 0], d3.interpolateMagma)
+        var color = d3.scaleLinear()
+            .domain([0, 5])
+            .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+            .interpolate(d3.interpolateHcl)
 
         const root = pack(data);
+
+        let focus = root;
+        let view;
+
         const svg = d3.select("#packing").append("svg")
-            .attr("viewBox", [0, 0, width, height])
-            .style("font", "10px sans-serif")
-            .attr("text-anchor", "middle");
+            .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
+            .style("display", "block")
+            // .style("margin", "0 -14px")
+            .style("background", color(0))
+            .style("cursor", "pointer")
+            .style("font", "14px sans-serif")
+            .attr("text-anchor", "middle")
+            .on("click", () => zoom(root));
 
-        const shadow = DOM.uid("shadow");
+        const node = svg.append("g")
+            .selectAll("circle")
+            .data(root.descendants().slice(1))
+            .join("circle")
+            .attr("fill", d => d.children ? color(d.depth) : "white")
+            .attr("pointer-events", d => !d.children ? "none" : null)
+            .on("mouseover", function() { d3.select(this).attr("stroke", "#000"); })
+            .on("mouseout", function() { d3.select(this).attr("stroke", null); })
+            .on("click", d => focus !== d && (zoom(d), d3.event.stopPropagation()));
 
-        svg.append("filter")
-            .attr("id", shadow.id)
-            .append("feDropShadow")
-            .attr("flood-opacity", 0.3)
-            .attr("dx", 0)
-            .attr("dy", 1);
+        const label = svg.append("g")
+            .style("font", "14px sans-serif")
+            .attr("pointer-events", "none")
+            .attr("text-anchor", "middle")
+            .selectAll("text")
+            .data(root.descendants())
+            .join("text")
+            .style("fill-opacity", d => d.parent === root ? 1 : 0)
+            .style("display", d => d.parent === root ? "inline" : "none")
+            .text(d => d.data.name);
 
-        const node = svg.selectAll("g")
-            .data(d3.nest().key(d => d.height).entries(root.descendants()))
-            .join("g")
-            .attr("filter", shadow)
-            .selectAll("g")
-            .data(d => d.values)
-            .join("g")
-            .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`);
+        zoomTo([root.x, root.y, root.r * 2]);
 
-        node.append("circle")
-            .attr("r", d => d.r)
-            .attr("fill", d => color(d.height));
+        function zoomTo(v) {
+            const k = width / v[2];
 
-        const leaf = node.filter(d => !d.children);
+            view = v;
 
-        leaf.select("circle")
-            .attr("id", d => (d.leafUid = DOM.uid("leaf")).id);
+            label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+            node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+            node.attr("r", d => d.r * k);
+        }
 
-        leaf.append("clipPath")
-            .attr("id", d => (d.clipUid = DOM.uid("clip")).id)
-            .append("use")
-            .attr("xlink:href", d => d.leafUid.href);
+        function zoom(d) {
+            const focus0 = focus;
 
-        leaf.append("text")
-            .attr("clip-path", d => d.clipUid)
-            .selectAll("tspan")
-            .data(d => d.data.name.split(/(?=[A-Z][a-z])|\s+/g))
-            .join("tspan")
-            .attr("x", 0)
-            .attr("y", (d, i, nodes) => `${i - nodes.length / 2 + 0.8}em`)
-            .text(d => d);
+            focus = d;
 
-        node.append("title")
-            .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
+            const transition = svg.transition()
+                .duration(d3.event.altKey ? 7500 : 750)
+                .tween("zoom", d => {
+                    const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+                    return t => zoomTo(i(t));
+                });
+
+            label
+                .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+                .transition(transition)
+                .style("fill-opacity", d => d.parent === focus ? 1 : 0)
+                .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+                .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+        }
     }
 }
 
